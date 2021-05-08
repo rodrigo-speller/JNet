7980fd244eb996d8c8b01d994048b59b848c52b8
Rodrigo Araújo Speller <rodrigo@primosti.com.br>
2021-05-08 16:49:32 -0300

    JNetConfiguration logging

--- src/JNet.Runtime/JNetConfiguration.cs
+++ src/JNet.Runtime/JNetConfiguration.cs
@@ -2,9 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
 
 using JNet.Runtime.InteropServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace JNet.Runtime
 {
@@ -14,8 +16,18 @@ namespace JNet.Runtime
         public IEnumerable<string> Classpath { get; set; }
         public IJNetBootstrap Bootstrap { get; set; }
         public JNIVersion JNIVersion { get; set; } = JNIVersion.Version10;
+        public Logger LoggerCallback { get; set; }
 
-        internal IEnumerable<JavaVMOption> BuildOptions()
+        public delegate void Logger(string format, IntPtr args);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int vfprintf_Contract(
+            IntPtr stream,
+            [In][MarshalAs(UnmanagedType.LPStr)] string format,
+            IntPtr args
+        );
+
+        internal unsafe IEnumerable<JavaVMOption> BuildOptions()
         {
             var options = new List<JavaVMOption>();
 
@@ -23,6 +35,18 @@ namespace JNet.Runtime
             {
                 var classpath = string.Join(Path.PathSeparator, Classpath);
                 options.Add(new JavaVMOption($"-Djava.class.path={classpath}"));
+            }
+
+            var logger = LoggerCallback;
+            if (logger is not null)
+            {
+                options.Add(new JavaVMOption("vfprintf")
+                {
+                    extraInfo = Marshal.GetFunctionPointerForDelegate<vfprintf_Contract>((stream, format, args) => {
+                        logger(format, args);
+                        return 0;
+                    }).ToPointer()
+                });
             }
 
             return options.AsEnumerable();
